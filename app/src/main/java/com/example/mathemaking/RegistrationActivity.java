a62b7118eeec2f2b3ce5d6c0e4d4fca6774ee949
Takatso Molekane <569869@students.wits.ac.za>
2018-11-30 11:04:31 +0200

    Registration complete.
    Signed-off-by: Takatso Molekane <569869@students.wits.ac.za>

--- app/src/main/java/com/example/mathemaking/RegistrationActivity.java
+++ app/src/main/java/com/example/mathemaking/RegistrationActivity.java
@@ -1,9 +1,29 @@
 package com.example.mathemaking;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegistrationActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener{
+    private FirebaseAuth firebaseAuth;
+    private EditText etUsername, etPassword;
+    private CheckBox checkBox;
+    private Button btnRegister;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +32,83 @@ public class RegistrationActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Registration");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        /*Initialize the views*/
+        initialize();
+
+        /*Check whether the user is already logged in*/
+        if(firebaseAuth.getCurrentUser() != null){
+            /*Start the Journal activity*/
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
+
+        btnRegister.setOnClickListener(this);
+
     }
+
+    private void initialize() {
+        etUsername = (EditText)findViewById(R.id.et_username);
+        etPassword = (EditText)findViewById(R.id.et_password);
+        checkBox = (CheckBox) findViewById(R.id.checkBx);
+        btnRegister = (Button)findViewById(R.id.btn_reg);
+        progressDialog = new ProgressDialog(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnRegister){
+            doRegistration();
+        }
+    }
+
+    private void doRegistration() {
+
+        String nameError = "Name field cannot be empty";
+        String emailError = "Email field cannot be empty";
+        String passwordError = "Password field cannot be empty";
+        String progressMessage = "Registration underway...";
+        Context context = getApplicationContext();
+
+        String username, password;
+
+        username = etUsername.getText().toString().trim();
+        password = etPassword.getText().toString().trim();
+
+        /*Validate input*/
+        if(TextUtils.isEmpty(username)){
+            Toast.makeText(context,emailError,Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(context,passwordError,Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        /*Straight to Firebase*/
+        progressDialog.setTitle("Registering user");
+        progressDialog.setMessage(progressMessage);
+        progressDialog.show();
+        firebaseAuth.createUserWithEmailAndPassword(username,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if(task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(),
+                                    "Successfully Registered",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(),
+                                    MainActivity.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(getApplicationContext(),
+                                    "Could not register, Please try again",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
 }
