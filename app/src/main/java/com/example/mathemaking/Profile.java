8b362ae3cda6e2952b163b12402d6ae1c51ae9a9
Takatso Molekane <569869@students.wits.ac.za>
2019-01-04 10:10:50 +0200

    UI Polishing underway.

--- app/src/main/java/com/example/mathemaking/Profile.java
+++ app/src/main/java/com/example/mathemaking/Profile.java
@@ -4,13 +4,22 @@ import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Calendar;
 
 public class Profile extends AppCompatActivity  implements View.OnClickListener {
 
-    private EditText etDOB;
+    private EditText etAge, etName, etSurname;
+    private RadioButton radMale, radFem;
+    private Button btnSend;
+    private DatabaseReference rootRef, mRoot;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +28,29 @@ public class Profile extends AppCompatActivity  implements View.OnClickListener 
         getSupportActionBar().setTitle("Update Profile");
         getSupportActionBar().setHomeButtonEnabled(true);
 
-        etDOB = findViewById(R.id.date_of_birth);
-        etDOB.setOnClickListener(this);
+        etName = findViewById(R.id.etName);
+        etSurname = findViewById(R.id.et_last_name);
+        etAge = findViewById(R.id.date_of_birth);
+        radMale = findViewById(R.id.radMale);
+        radFem = findViewById(R.id.radFem);
+        btnSend = findViewById(R.id.btnSave);
+        btnSend.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        if(view == etDOB){
+        if(view == btnSend){
+            String name  = etName.getText().toString().trim();
+            String surname = etSurname.getText().toString().trim();
+            int age = Integer.parseInt(etAge.getText().toString().trim());
+            rootRef = FirebaseDatabase.getInstance().getReference();
+            mRoot = rootRef.child("Profile");
 
-
+            mRoot.push().setValue(name);
+            mRoot.push().setValue(surname);
+            mRoot.push().setValue(age);
 
         }
+
     }
 }
